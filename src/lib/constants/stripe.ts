d2ed5cd1f5ebb9173604569afed2a8745158764f
pixelholiday-design <pixelholiday@gmail.com>
2026-03-23 17:00:01 +0100

    Add src/lib/constants/stripe.ts

---
+++ src/lib/constants/stripe.ts
@@ -0,0 +1,3 @@
+// Stripe constants
+export const STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '';
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY || '';
