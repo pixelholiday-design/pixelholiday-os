135f40e0fe7a32c1d9ac2e73b87d5d8ff166d329
pixelholiday-design <pixelholiday@gmail.com>
2026-03-24 00:29:22 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+// Configure the Cloudflare development platform bindings in dev mode
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
